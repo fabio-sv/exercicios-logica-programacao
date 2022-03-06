57769ace060a06f80164a05438517ab9c1a3fca0
fabio-sv <fabio_dev@outlook.com>
2022-03-06 16:10:06 -0300

    update

--- HacherHank/staircase.js
+++ HacherHank/staircase.js
@@ -1,21 +1,19 @@
 // https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=false&h_r=next-challenge&h_v=zen
-
 function staircase(n) {
-    let row = [];
-    let position = n;
-    let str
+    let row = [],
+        position = n,
+        txt,
+        str;
 
     for (let i = 1; i <= n; i++) {
-        // console.log('row ' + i + ':' + position)
-
         for (let j = 1; j <= n; j++) {
             j < position ? row.push(" ") : row.push("#");
         }
         position = position - 1;
 
-        str = row.toString()
-
-        console.log("row:", str);
+        txt = row.toString();
+        str = txt.replace(/,/g, "");
+        console.log(str);
 
         for (let i = 0; i < n; i++) {
             row.pop();
@@ -23,4 +21,14 @@ function staircase(n) {
     }
 }
 
-staircase(4);
+staircase(10);
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// ########
+// #########
+// ##########
